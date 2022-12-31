e1d51ab75df57ec734e7fe3046f5132fe0631e7b
Ido Calman <calman.ido@gmail.com>
2022-12-31 13:24:50 +0100

    Adds fold to all REs

--- parse.py
+++ parse.py
@@ -1,5 +1,32 @@
+import itertools
+import string
+
 DATA_FILE = 'data/restriction_enzymes.txt'
-if __name__ == '__main__':
+
+symbols = {
+    'A': ['A'],
+    'G': ['G'],
+    'C': ['C'],
+    'T': ['T'],
+    'R': ['G', 'A'],
+    'Y': ['C', 'T'],
+    'K': ['G', 'T'],
+    'M': ['A', 'C'],
+    'S': ['G', 'C'],
+    'W': ['A', 'T'],
+    'B': ['G', 'T', 'C'],
+    'D': ['G', 'A', 'T'],
+    'H': ['A', 'C', 'T'],
+    'V': ['G', 'C', 'A'],
+    'N': ['A', 'G', 'C', 'T']
+}
+
+
+def strip_punct(x):
+    return x.translate(str.maketrans('', '', string.punctuation))
+
+
+def parse(filename: str, expand=True):
     entries = []
     curr_entry_dna = None
     curr_entry_aa = ''
@@ -16,9 +43,34 @@ if __name__ == '__main__':
                 if line == '\n':
                     # add new entry
                     if curr_entry_dna and len(curr_entry_aa):
+                        curr_entry_dna = strip_punct(curr_entry_dna)
                         entries.append((curr_entry_dna, curr_entry_aa))
                         curr_entry_dna = None
                         curr_entry_aa = ''
                 else:
                     if curr_entry_dna:
                         curr_entry_aa += ''.join(line.split())
+
+    # expand multiple binding DNA seqs to individual samples
+    if expand:
+        new_entries = []
+        for entry in entries:
+            # collate all possible basepairs
+            entry_possible_lists = []
+            dna, aa = entry
+            for x in dna:
+                entry_possible_lists.append(symbols[x])
+
+            # calculate all combinatorial sequences
+            entry_dna_seqs = itertools.product(*entry_possible_lists)
+            entry_dna_seqs = [''.join(x) for x in entry_dna_seqs]
+            new_entries += [(dna_seq, aa) for dna_seq in entry_dna_seqs]
+            entry_possible_lists = []
+
+        entries = new_entries
+
+    return entries
+
+
+if __name__ == '__main__':
+    entries = parse(DATA_FILE)
