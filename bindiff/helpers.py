e6d421c64297823fd47da0f88d87125d455283f9
Ido Calman <calman.ido@gmail.com>
2023-03-14 14:40:01 -0400

    Code cleanup

---
+++ bindiff/helpers.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn.functional as F
+
+
+def exists(val):
+    return val is not None
+
+
+def max_neg_value(t):
+    return -torch.finfo(t.dtype).max
+
+
+def default(val, d):
+    return val if exists(val) else d
+
+
+def l2norm(t):
+    return F.normalize(t, dim = -1)
+
+
+def batched_index_select(values, indices, dim = 1):
+    value_dims = values.shape[(dim + 1):]
+    values_shape, indices_shape = map(lambda t: list(t.shape), (values, indices))
+    indices = indices[(..., *((None,) * len(value_dims)))]
+    indices = indices.expand(*((-1,) * len(indices_shape)), *value_dims)
+    value_expand_len = len(indices_shape) - (dim + 1)
+    values = values[(*((slice(None),) * dim), *((None,) * value_expand_len), ...)]
+
+    value_expand_shape = [-1] * len(values.shape)
+    expand_slice = slice(dim, (dim + value_expand_len))
+    value_expand_shape[expand_slice] = indices.shape[expand_slice]
+    values = values.expand(*value_expand_shape)
+
+    dim += value_expand_len
+    return values.gather(dim, indices)
