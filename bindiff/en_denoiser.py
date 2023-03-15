41adb544d37fe26ebb2015ec0592252b16e0d7b6
Ido Calman <calman.ido@gmail.com>
2023-03-15 18:33:28 -0400

    Fixes HUGE beta defaults bug

--- bindiff/en_denoiser.py
+++ bindiff/en_denoiser.py
@@ -206,9 +206,9 @@ class EnDenoiser(pl.LightningModule):
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
-        parser.add_argument('--lr', type=float, default=0.0001)
-        parser.add_argument('--beta_small', type=float, default=0.02)
-        parser.add_argument('--beta_large', type=float, default=0.2)
+        parser.add_argument('--lr', type=float, default=1e-4)
+        parser.add_argument('--beta_small', type=float, default=2e-4)
+        parser.add_argument('--beta_large', type=float, default=0.02)
         parser.add_argument('--dim', type=int, default=64)
         parser.add_argument('--dim_head', type=int, default=64)
         parser.add_argument('--depth', type=int, default=8)
