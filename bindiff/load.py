c7998c5fd822da5d9a93b2a6b5f322bc7df8eb5f
Ido Calman <calman.ido@gmail.com>
2023-03-25 20:11:43 -0400

    Removes old files

--- bindiff/load.py
+++
@@ -1,7 +0,0 @@
-import sidechainnet as scn
-from collate import prepare_dataloaders
-
-if __name__ == "__main__":
-    bs = 4
-    data = scn.load(12, thinning=30, batch_size=bs)
-    data_loaders = prepare_dataloaders(data, True, batch_size=bs)
