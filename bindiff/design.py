fec535a8c65a9e0f24f9b4133c9940421ebcd2d4
Ido Calman <calman.ido@gmail.com>
2023-05-04 23:27:08 -0400

    Add ESMFold to pipeline

--- bindiff/design.py
+++ bindiff/design.py
@@ -1,9 +1,13 @@
 import os
 import subprocess
 from biotite.sequence.io import fasta
+import torch
+import esm
 
 
 OUTPUT_PATH = "pipeline"
+model = esm.pretrained.esmfold_v1()
+model = model.eval().cuda()
 
 
 def process(args):
@@ -58,23 +62,7 @@ def mpnn(jsonl_path):
         'ProteinMPNN/test.jsonl',
         '--ca_only'
     ]
-
-    ret = -1
-    num_tries = 0
-    while ret < 0:
-        try:
-            process = subprocess.Popen(
-                pmpnn_args,
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.STDOUT
-            )
-            ret = process.wait()
-        except Exception as e:
-            num_tries += 1
-            print(f'Failed ProteinMPNN. Attempt {num_tries}/5')
-            if num_tries > 4:
-                raise e
-
+    process(pmpnn_args)
     fname = fname_from_path(jsonl_path)
     out_fasta = os.path.join(out_dir, 'seqs', f'{fname}.fa')
     print(f"Produced sequences in file {out_fasta}")
@@ -82,7 +70,20 @@ def mpnn(jsonl_path):
 
 
 def esmfold(fasta_path):
-    fasta_seqs = fasta.FastaFile.read(mpnn_fasta_path)
+    out_path = os.path.join(OUTPUT_PATH, 'out.pdb')
+    fasta_seqs = fasta.FastaFile.read(fasta_path)
+
+    # run ESMFold on all sequences in FASTA
+    for i, (header, string) in enumerate(fasta_seqs.items()):
+        with torch.no_grad():
+            output = model.infer_pdb(string)
+
+        with open(out_path, "w") as f:
+            f.write(output)
+
+    print("Folded structure saved to: {out_path}")
+    return out_path
+
 
 def pipeline(pdb_path):
     # process PDB file into jsonl for MPNN
@@ -90,6 +91,7 @@ def pipeline(pdb_path):
     # run MPNN to get sequences
     sequence_fasta = mpnn(processed_file)
     # use ESMFold to fold sequences
+    esmfold(sequence_fasta)
 
 
 if __name__ == "__main__":
